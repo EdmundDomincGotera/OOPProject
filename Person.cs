2b9546c2ca0dd887efc4357b5df81e03cfaddd73
EdmundDomincGotera <43458604+EdmundDomincGotera@users.noreply.github.com>
2018-11-23 09:18:53 +0800

    Added Files
    Added Calculations.cs, Person.cs, Product.cs

---
+++ Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Person
+    {
+        public string FirstName;
+        public string MiddleInitial;
+        public string LastName;
+        public string Birthday;
+        public string Address;
+
+        public string GetFullName(string firstName, string middleInitial, string lastName)
+        {
+            return "";
+        }
+
+        static int GetAge()
+        {
+            return 0;
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Product
+    {
+        public string Name;
+        public string Description;
+        public decimal Price;
+        public string Manufacturer;
+        public int Items;
+
+        static void DeductItems(int items)
+        {
+            
+        }
+    }
+}
