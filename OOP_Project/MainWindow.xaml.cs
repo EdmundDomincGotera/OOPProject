ca36a334f2cb17dd8943e51235fb965de4f987c8
EdmundDomincGotera <43458604+EdmundDomincGotera@users.noreply.github.com>
2018-12-07 09:37:48 +0800

    Add Transaction window and main window
    Added calc.cs, code to transfer values within windows

---
+++ OOP_Project/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOP_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Transaction_Window transwin = new Transaction_Window();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Transaction_Window transwin = new Transaction_Window();
+
+            transwin.Customer = new Person(this.txtFirstName.Text, this.txtLastName.Text, this.txtMiddleName.Text);
+            transwin.Customer.Birthday = this.dpBirthday.Text;
+            transwin.Customer.Address = this.txtAddress.Text;
+
+            transwin.Jewelry = new Product();
+            transwin.Jewelry.Name = this.cmbJewelry.Text;
+            transwin.Show();
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        
+    }
+}
---
+++ OOP_Project/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Project
+{
+    public class Person
+    {
+        public string FirstName;
+        public string MiddleName;
+        public string LastName;
+        public string Birthday;
+        public string Address;
+
+        public Person(string firstName, string lastName, string middleName = " ")
+        {
+            this.FirstName = firstName;
+            this.MiddleName = middleName;
+            this.LastName = lastName;
+        }
+        public string GetFullName()
+        {
+            FirstName = System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(FirstName.ToLower());
+            MiddleName = System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(MiddleName.ToLower());
+            LastName = System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(LastName.ToLower());
+            return FirstName + " " + LastName + ". " + MiddleName;
+        }
+
+        public int GetAge()
+        {
+            return Calculations.CalculateAge(Birthday, true);
+        }
+    }
+}
