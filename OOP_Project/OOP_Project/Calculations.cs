5b670373eab6fb29106380d9a1e71fac60b5096c
EdmundDomincGotera <43458604+EdmundDomincGotera@users.noreply.github.com>
2018-12-10 09:25:21 +0800

    Added new windows
    Unfinished windows

---
+++ OOP_Project/OOP_Project/Calculations.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Project
+{
+    public class Calculations
+    {
+        public static int CalculateAge(string birthDate, bool returnInMonths = false)
+        {
+            int age;
+
+            DateTime now = DateTime.UtcNow;
+            DateTime past = Convert.ToDateTime(birthDate);
+
+
+            if (now.Year > past.Year)
+            {
+                age = (now.Year - past.Year);
+
+                if (returnInMonths)
+                {
+                    age = age * 12;
+                }
+                age += (now.Month - past.Month);
+                return age;
+            }
+            else
+            {
+                age = (past.Year - now.Year);
+                if (age == 1)
+                {
+                    returnInMonths = false;
+                }
+                if (returnInMonths)
+                {
+                    age = age * 12;
+                }
+                age = (now.Month - past.Month);
+                return age;
+            }
+        }
+
+        public static decimal CalculateInterest(decimal principalAmount, decimal monthlyRate, int items)
+        {
+            principalAmount = principalAmount * items;
+            decimal interest = principalAmount * (monthlyRate / 100);
+            return interest;
+        }
+        public static decimal CalculateAccruedAmount(decimal principalAmount, decimal monthlyRate, int items, string date)
+        {
+            return (principalAmount * items) + CalculateInterest(principalAmount, monthlyRate, items) * CalculateAge(date, true);
+        }
+    }
+}
+
---
+++ OOP_Project/OOP_Project/Transaction_Window.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OOP_Project
+{
+    /// <summary>
+    /// Interaction logic for Transaction_Window.xaml
+    /// </summary>
+    public partial class Transaction_Window : Window
+    {
+        public Person Customer;
+        public Product Jewelry;
+        public Transaction_Window()
+        {
+            InitializeComponent();
+        }
+
+        //private void Window_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    lstDetails.Items.Add(Customer.GetFullName() + " " + Jewelry.Name);
+        //}
+
+        private void TransClick (object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
